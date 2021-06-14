d83510a2c6a6e29444346fd71bb1495ae19b8319
Kris Lyon <kris.lyon@gmail.com>
2021-06-13 22:21:59 -0400

    Removed old python solutions, switching to use JS.

--- series1/convert.py
+++
@@ -1,8 +0,0 @@
-def convert_array_to_hex( data ):
-    return ''.join([format(v,'02x') for v in data])
-
-def convert_hex_to_array( data ):
-    return bytearray.fromhex( data.strip() )
-
-def convert_array_to_ascii( data ):
-    return ''.join(chr(i) for i in data )
